183c03654c402321951b53049695bf1d606d04ed
KOHEEJI <sglee@cudo.co.kr>
2023-03-10 14:23:23 +0900

    안전하고 단순하게 구현하는 방법 (enum 사용)

--- src/main/java/com/study/designpatterns/singleton/App.java
+++ src/main/java/com/study/designpatterns/singleton/App.java
@@ -2,8 +2,8 @@ package com.study.designpatterns.singleton;
 
 public class App {
     public static void main(String[] args){
-        Settings settings = Settings.getInstance();
-        Settings settings1 = Settings.getInstance();
+        Settings settings = Settings.INSTANCE;
+        Settings settings1 = Settings.INSTANCE;
         System.out.println(settings1 == settings);
 
     }
--- src/main/java/com/study/designpatterns/singleton/Settings.java
+++ src/main/java/com/study/designpatterns/singleton/Settings.java
@@ -1,17 +1,5 @@
 package com.study.designpatterns.singleton;
 
-public class Settings {
-
-    // 스레드 세이프하게 Static inner 클래스를 사용
-
-    private Settings(){}
-
-    private static class SettingsHolder{
-        private static final Settings INSTANCE = new Settings();
-    }
-
-    public static Settings getInstance(){
-        return SettingsHolder.INSTANCE;
-    }
-
+public enum Settings {
+    INSTANCE;
 }
